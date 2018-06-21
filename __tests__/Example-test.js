2f7840eb1192939b9d4d50aa84b990f37f0f575c
Beren Erchamion <beren@houseofbeor.net>
2018-06-21 13:12:48 -0400

    Adding unit test and redux-mock-store

--- __tests__/Example-test.js
+++ __tests__/Example-test.js
@@ -1,4 +1,15 @@
+import 'react-native'
+import React from 'react'
+import AddPost from '../components/AddPost'
+import renderer from 'react-test-renderer'
+
 it('works', () => {
     expect(1).toBe(1);
   })
-  
+
+it('renders correctly', () => {
+const tree = renderer.create(
+    <AddPost />
+    ).toJSON();
+expect(tree).toMatchSnapshot();
+});
