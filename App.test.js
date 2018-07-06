fd1396b19c7af2f01311ca4e85e925399cd1c4ad
Beren Erchamion <beren@houseofbeor.net>
2018-07-06 08:17:34 -0400

    Added support for unit testing and some unit tests. Ignored the output of unit tests - no need to have this in the repo.

--- App.test.js
+++ App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import App from './App';
+import configureStore from 'redux-mock-store'
+import { shallow, mount, render } from 'enzyme'
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 import renderer from 'react-test-renderer';
 
+const middlewares = [];
+const mockStore = configureStore(middlewares)
+
+configure ({adapter: new Adapter()})
+
+const initialState = {
+    posts: [],
+}
+
 it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
-});
+  const wrapper = shallow(
+      <App />,
+      {context: {store: mockStore(initialState) } },
+  )
+  expect(wrapper.dive()).toMatchSnapshot()
+})
